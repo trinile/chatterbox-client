b0f788a9fb75c302aa0ffccad2cce82f6ff6ee5b
trinile <trinile@gmail.com>
2016-03-14 16:24:30 -0700

    Network issue problem

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,44 @@
 // YOUR CODE HERE:
 
+var app = {};
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.init = function() {
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    success: function(data) {
+      console.log('success');
+      console.log(data);
+    },
+    error: function(error) {
+      console.error(error);
+    }
+  });
+  console.log('hello');
+};
+
+
+var successFunction = function() {
+  //append to html
+
+};
