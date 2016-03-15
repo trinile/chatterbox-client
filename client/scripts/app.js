624ada8a5a7edc2e8d9d88b6f2648159c522fc07
trinile <trinile@gmail.com>
2016-03-14 17:41:44 -0700

    working GET

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,14 +25,62 @@ app.fetch = function() {
   $.ajax({
     url: app.server,
     type: 'GET',
-    success: function(data) {
-      console.log('success');
-      console.log(data); 
-    },
+    success: function(data) { appendChatBox(data); },
     error: function(error) {
       console.error(error);
     }
   });
-  console.log('hello');
 };
+
+app.fetch();
+
+//createdAt:
+//objectId:
+//roomname:
+//username
+//text
+
+// var chatBox = function(data) {
+//   //html for messages
+//   var html = '<div><span>' + data.username + '</span>'
+//             + '<span>' + data.text + 
+//             '</span></div>';
+
+//   return html;
+// }
+
+var escapeHTML = function(text) {
+  if (text === undefined) {
+    return 'no message~~~~';
+  }
+  return text.replace(/[-[\]{}*+\\^$|#\s<>"]/g, '\\$&');
+};
+
+var appendChatBox = function(data) {
+  var arr = data.results;
+  _.each(arr, function(message) {
+    var user = message.username;
+    var text = message.text;
+    // escapeHTML(user);
+    // escapeHTML(text);
+    var html = '<div class="chat"><span class="username">' + escapeHTML(user) + '</span>'
+            + '<span class="text">' + escapeHTML(text) + 
+            '</span></div>';
+    
+    console.log('user: ' + escapeHTML(user), 'text: ', escapeHTML(text));
+
+    $('#chats').prepend(html);
+  });
+
+  setTimeout(function() {
+    app.fetch();
+  },5000);
+};
+
+
+//Escaping &, <, >, ", ', `, , !, @, $, %, (, ), =, +, {, }, [, and ] is almost enough
+
+
+
+
 
